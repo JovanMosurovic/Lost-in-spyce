3e711efc5e3d5a7c3e51b3ca521c79ad4d6a225c
Jovan <mosurovicjovan@gmail.com>
2024-11-09 01:38:02 +0100

    Initial commit.

---
+++ algorithms.py
@@ -0,0 +1,17 @@
+import random
+
+
+class Algorithm:
+    def get_path(self, state):
+        pass
+
+
+class ExampleAlgorithm(Algorithm):
+    def get_path(self, state):
+        path = []
+        while not state.is_goal_state():
+            possible_actions = state.get_legal_actions()
+            action = possible_actions[random.randint(0, len(possible_actions) - 1)]
+            path.append(action)
+            state = state.generate_successor_state(action)
+        return path
