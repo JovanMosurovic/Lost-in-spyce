6f87fb36e974fbc3321e312b376ffd2676a3ebec
Jovan <mosurovicjovan@gmail.com>
2024-11-11 22:27:53 +0100

    Implemented Branch and bound algorithm.

--- algorithms.py
+++ algorithms.py
@@ -52,6 +52,32 @@ class Red(Algorithm):
                     my_queue.insert(0, (next_state, path + [action]))
         return None
 
+class Black(Algorithm):
+    def get_path(self, state): # Branch and bound
+        paths = [(state, [], 0)]
+        visited = set()
+        visited.add(state.get_state('S'))
+
+        while paths:
+            current_state, path, current_cost = paths.pop(0)
+
+            if current_state.is_goal_state():
+                return path
+
+            for action in current_state.get_legal_actions():
+                next_state = current_state.generate_successor_state(action)
+                next_state_key = next_state.get_state('S')
+
+                if next_state_key not in visited:
+                    visited.add(next_state_key)
+                    action_cost = current_state.get_action_cost(action)
+                    total_cost = current_cost + action_cost
+
+                    paths.append((next_state, path + [action], total_cost))
+
+            paths.sort(key=lambda x: x[2])
+
+        return None
 
 
 
